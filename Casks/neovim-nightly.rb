bd1db36afc2b847317e155a0e72b2ccc734234a8
yuanmich2 <160265179+yuanmich2@users.noreply.github.com>
2026-09-21 13:56:13 -0700

    Update neovim-nightly.rb

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -3,7 +3,7 @@ cask "neovim-nightly" do
   sha256 :no_check
 
   arch arm: "arm64", intel: "x86_64"
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz"
 
   name "Neovim"
   desc "Vim-fork focused on extensibility and usability"
